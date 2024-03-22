936c46a11a0ba06b74c957b07126855bf1c908a2
Matheus Dias <matheusdiasmello@icloud.com>
2024-03-22 14:35:10 -0300

    feat: implemented bcrypt in create user

--- src/use-cases/create-user.usecase.ts
+++ src/use-cases/create-user.usecase.ts
@@ -1,26 +1,31 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { CreateUserDTO } from '../dtos/create-user.dto';
 import { User } from '../entities/user.entity';
 import { prismaClient } from '../database/prisma-client';
 import { UserlreadyExistsError } from './errors/user-already-exists';
+import bcrypt from 'bcrypt';
 
 export class CreateUserUseCase {
-  static async execute({ name, email, password}: CreateUserDTO): Promise<void> {
-      const userAlreadyExists = await prismaClient.user.findFirst({
-        where: {
-          email,
-        },
-      });
+  static async execute({
+    name,
+    email,
+    password,
+  }: CreateUserDTO): Promise<void> {
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email,
+      },
+    });
 
-      if (userAlreadyExists) {
-        throw new UserlreadyExistsError(email);
-      }
+    if (userAlreadyExists) {
+      throw new UserlreadyExistsError(email);
+    }
 
-      const user = new User(name, email, password);
+    const hashPassword = await bcrypt.hash(password, 10);
 
-      await prismaClient.user.create({
-        data: user,
-      });
-    } 
+    const user = new User(name, email, hashPassword);
+
+    await prismaClient.user.create({
+      data: user,
+    });
   }
-
+}
